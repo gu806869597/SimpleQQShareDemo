98881c0036eb4865069626bc2613cdbf92164470
guhaoran <806869597@qq.com>
2016-08-03 15:46:38 +0800

    已经将ShareSDK替换为原生的TencentSDK

--- app/src/main/java/cc/jimblog/simpleqqsharedemo/MainActivity.java
+++ app/src/main/java/cc/jimblog/simpleqqsharedemo/MainActivity.java
@@ -1,11 +1,13 @@
 package cc.jimblog.simpleqqsharedemo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Handler.Callback;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -15,26 +17,33 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.mob.tools.utils.UIHandler;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.common.Constants;
+import com.tencent.connect.share.QQShare;
+import com.tencent.connect.share.QzoneShare;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import cn.sharesdk.framework.Platform;
-import cn.sharesdk.framework.PlatformActionListener;
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.tencent.qq.QQ;
-import cn.sharesdk.tencent.qzone.QZone;
 
 /**
+ * @author JimHao
+ * @version 1.1 第二次重新修改
  *
  */
-public class MainActivity extends AppCompatActivity implements Callback, PlatformActionListener {
+public class MainActivity extends AppCompatActivity {
     //这里是注解框架
     @BindView(R.id.nickname)
     TextView nickName;
@@ -48,231 +57,296 @@ public class MainActivity extends AppCompatActivity implements Callback, Platfor
     Button shareToQzone;
     @BindView(R.id.logOut_to_qq)
     Button logOutToQq;
-
-    private static final int IS_USER = 1;           //用户已经登陆了
-    private static final int USER_LOGIN = 2;        //登陆完成
-    private static final int USER_CANCEL = 3;       //登录被取消
-    private static final int USER_ERROR = 4;        //登录错误
-    private static final int USER_OK = 5;           //认证完成
+    @BindView(R.id.show_user_info)
+    TextView showUserInfo;
+    private UserInfo mInfo = null;  //用户类
+    private static Tencent mTencent;  //主类
+    private static final String APP_KEY = "222222"; //用户信息主键
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this); //注解初始化
-        ShareSDK.initSDK(this); //初始化SDK
-
+        if (mTencent == null) {
+            mTencent = Tencent.createInstance(APP_KEY, this);
+        }
     }
 
-    @OnClick({R.id.login_to_qq, R.id.share_to_qq, R.id.share_to_qzone})
+    @OnClick({R.id.login_to_qq, R.id.share_to_qq, R.id.share_to_qzone, R.id.logOut_to_qq})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.login_to_qq:
-                authorize(new QQ(this));    //登录方法
+                if (!mTencent.isSessionValid()) {
+                    mTencent.login(this, "all", loginListener);
+                    Log.d("SDKQQAgentPref", "FirstLaunch_SDK:" + SystemClock.elapsedRealtime());
+                }else{
+                    Toast.makeText(MainActivity.this, "用户已经登陆了", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.share_to_qq:
                 shareToQQ();
+                break;
             case R.id.share_to_qzone:
-                setShareToQzone();
+                shareToQzone();
+                break;
+            case R.id.logOut_to_qq:
+                if (mTencent.isSessionValid()) {
+                    mTencent.logout(this);
+                    Toast.makeText(MainActivity.this, "退出登陆成功", Toast.LENGTH_SHORT).show();
+                    return ; 
+                }
+                Toast.makeText(MainActivity.this, "用户还没有登录", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    /**
+     * 处理登录完成的监听
+     */
+    IUiListener loginListener = new BaseUiListener() {
+        @Override
+        protected void doComplete(JSONObject values) {
+            Log.d("SDKQQAgentPref", "AuthorSwitch_SDK:" + SystemClock.elapsedRealtime());
+            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+            initOpenidAndToken(values);
+            updateUserInfo();
+        }
+    };
+
+    /**
+     * @param jsonObject  成功回调得到的jsonObject
+     */
+    public void initOpenidAndToken(JSONObject jsonObject) {
+        try {
+            String token = jsonObject.getString(Constants.PARAM_ACCESS_TOKEN);
+            String expires = jsonObject.getString(Constants.PARAM_EXPIRES_IN);
+            String openId = jsonObject.getString(Constants.PARAM_OPEN_ID);
+            String time = "" + (System.currentTimeMillis() + Long.parseLong(expires) * 1000);
+            if (!TextUtils.isEmpty(token) && !TextUtils.isEmpty(expires)
+                    && !TextUtils.isEmpty(openId)) {
+                mTencent.setAccessToken(token, expires);
+                mTencent.setOpenId(openId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //得到用户信息的方法
+    private void updateUserInfo() {
+        if (mTencent != null && mTencent.isSessionValid()) {
+            class BaseUIListener implements IUiListener {
+                private String mScope;
+                public BaseUIListener(String mScope) {
+                    super();
+                    this.mScope = mScope;
+                }
+                @Override
+                public void onError(UiError e) {
+                    //发生错误
+                }
+                @Override
+                public void onComplete(final Object response) {
+                    Message msg = new Message();
+                    msg.obj = response;
+                    msg.what = 0;
+                    mHandler.sendMessage(msg);
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            JSONObject json = (JSONObject) response;
+                            if (json.has("figureurl")) {
+                                Bitmap bitmap = null;
+                                try {
+                                    bitmap = getbitmap(json.getString("figureurl_qq_2"));
+                                } catch (JSONException e) {
+
+                                }
+                                Message msg = new Message();
+                                msg.obj = bitmap;
+                                msg.what = 1;
+                                mHandler.sendMessage(msg);
+                            }
+                        }
+                    }.start();
+                }
+                @Override
+                public void onCancel() {
+                    //用户取消了
+                }
+            }
+            BaseUIListener listener = new BaseUIListener("get_simple_userinfo");
+            mInfo = new UserInfo(this, mTencent.getQQToken());  //这两个方法为必要的
+            mInfo.getUserInfo(listener);
+        }
+    }
+
+    /**
+     * 处理得到的用户信息的Handler
+     */
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
+                JSONObject response = (JSONObject) msg.obj;
+                String rmsg = response.toString().replace(",", "\n");
+                if (response.has("nickname")) {
+                    try {
+                        showUserInfo.setText(response.toString());
+                        nickName.setText(response.getString("nickname"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (msg.what == 1) {
+                Bitmap bitmap = (Bitmap) msg.obj;
+                headimage.setImageBitmap(bitmap);
+            }
+        }
+    };
+
+    /**
+     * 从网络下载图片
+     * @param imageUri
+     * @return
+     */
+    public static Bitmap getbitmap(String imageUri) {
+        // 显示网络上的图片
+        Bitmap bitmap = null;
+        try {
+            URL myFileUrl = new URL(imageUri);
+            HttpURLConnection conn = (HttpURLConnection) myFileUrl
+                    .openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+            //Log.v(TAG, "image download finished." + imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return bitmap;
+    }
+
+    /**Activity回调
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == Constants.REQUEST_LOGIN ||
+                resultCode == Constants.ACTIVITY_OK) {
+            Tencent.onActivityResultData(requestCode,resultCode,data,loginListener);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     /**
      * 分享到QQ
      */
     public void shareToQQ(){
-        QQ.ShareParams sp = new QQ.ShareParams();   //生成一个QQParams变量
-        sp.setTitle("JimHao的个人博客");
-        sp.setText("听风小栈\t记录在指间的冒险物语");
-        sp.setImageUrl("http://121.41.82.6/wordpress/w3g.png");//网络图片rul
-        sp.setTitleUrl("http://121.41.82.6/wordpress/");  //网友点进链接后，可以看到分享的详情
-        //3、非常重要：获取平台对象
-        Platform qq = ShareSDK.getPlatform(QQ.NAME);
-        qq.setPlatformActionListener(MainActivity.this); // 设置分享事件回调,如果不设置回调则传入Activity对象
-        // 执行分享
-        qq.share(sp);
+        Bundle bundle = new Bundle();
+        bundle.clear();
+        bundle.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE,
+                QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        bundle.putInt(QQShare.SHARE_TO_QQ_EXT_INT,
+                QQShare.SHARE_TO_QQ_FLAG_QZONE_ITEM_HIDE);
+        bundle.putString(QQShare.SHARE_TO_QQ_TITLE, "JimHao的个人博客");
+        bundle.putString(QQShare.SHARE_TO_QQ_SUMMARY, "记录在指间的冒险物语");
+        bundle.putString(
+                QQShare.SHARE_TO_QQ_TARGET_URL,"http://121.41.82.6/wordpress/");
+        ArrayList<String> imageUrls = new ArrayList<String>();
+        imageUrls.add("http://121.41.82.6/wordpress/w3g.png");
+        imageUrls.add("http://121.41.82.6/wordpress/w3g.png");
+//	        bundle.putStringArrayList(QQShare.SHARE_TO_QQ_IMAGE_URL, imageUrls);
+        bundle.putStringArrayList(QQShare.SHARE_TO_QQ_IMAGE_URL,imageUrls);
+        mTencent.shareToQQ(this, bundle, new BaseUiListener());
     }
 
     /**
-     * 分享到QQ空间
+     * 分享到QQ空间的方法
      */
-    public void setShareToQzone(){
-        QZone.ShareParams sp = new QZone.ShareParams();
-        sp.setTitle("JimHao的个人博客");
-        sp.setText("听风小栈\t记录在指间的冒险物语");
-        sp.setImageUrl("http://121.41.82.6/wordpress/w3g.png");//网络图片rul
-        sp.setTitleUrl("http://121.41.82.6/wordpress/");  //网友点进链接后，可以看到分享的详情
-        Platform qzone = ShareSDK.getPlatform(QZone.NAME);
-        qzone.setPlatformActionListener(MainActivity.this);
-        qzone.share(sp);
+    public void shareToQzone(){
+        final Bundle params = new Bundle();
+        params.putInt(QzoneShare.SHARE_TO_QZONE_KEY_TYPE,QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT );
+        params.putString(QzoneShare.SHARE_TO_QQ_TITLE, "JimHao的个人博客");
+        params.putString(QzoneShare.SHARE_TO_QQ_TARGET_URL, "http://121.41.82.6/wordpress/");
+        params.putString(QzoneShare.SHARE_TO_QQ_SUMMARY, "记录在指间的冒险物语");
+        ArrayList<String> imageUrls = new ArrayList<String>();
+        imageUrls.add("http://121.41.82.6/wordpress/w3g.png");
+        imageUrls.add("http://www.opensnap.com.cn/images/300x250.jpg");
+        params.putStringArrayList(QzoneShare.SHARE_TO_QQ_IMAGE_URL, imageUrls);
+        doShareToQzone(params); //这里将封装好的参数发送出去
     }
-    //登录完成回调
-    @Override
-    public void onComplete(Platform platform, int action, HashMap<String, Object> hashMap) {
-        if (action == Platform.ACTION_USER_INFOR) {
-            UIHandler.sendEmptyMessage(USER_OK, this);
-            login(platform.getName(), platform.getDb().getUserId(), hashMap);
-            Log.i("Brust Link", hashMap.toString());
-        }
-    }
-
-    @OnClick(R.id.logOut_to_qq)
-    public void onClick() {
-        Platform pf3=ShareSDK.getPlatform(MainActivity.this,QQ.NAME);
-        if (pf3.isValid()) {
-            pf3.removeAccount();//删除授权信息
-            Toast.makeText(this,"退出成功，测试请点击上方登陆按钮，如果出现重新登陆界面则算成功", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(MainActivity.this, "用户尚未登陆", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onError(Platform platform, int action, Throwable throwable) {
-        if (action == Platform.ACTION_USER_INFOR) {   //当用户授权发送错误时
-            UIHandler.sendEmptyMessage(USER_ERROR, this);
-        }
-        throwable.printStackTrace();
-    }
-
-    @Override
-    public void onCancel(Platform platform, int i) {
-        if (i == Platform.ACTION_USER_INFOR) {    //当用户取消操作时
-            UIHandler.sendEmptyMessage(USER_CANCEL, this);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ShareSDK.stopSDK(this); //当Activity关闭时关闭ShareSDK的服务
-    }
-    //在Handler中对消息类型进行处理
-    @Override
-    public boolean handleMessage(Message message) {
-        switch (message.what) {
-            case IS_USER:
-                Toast.makeText(this, "用户信息已存在，正在跳转登录操作…", Toast.LENGTH_SHORT).show();
-                break;
-            case USER_LOGIN:
-                //处理登陆后返回的消息
-                Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_SHORT).show();
-                HashMap<String, Object> userInfo = (HashMap<String, Object>) message.obj;
-                String nicknameStr = (String) userInfo.get("nickname");
-                String urlStr = (String) userInfo.get("figureurl_qq_1");
-                Log.i("URLStr",urlStr);
-                getUserBitmapThread thread = new getUserBitmapThread(urlStr);
-                thread.start();
-                nickName.setText(nicknameStr);
-                break;
-            case USER_CANCEL:
-                Toast.makeText(this, "授权操作已取消", Toast.LENGTH_SHORT).show();
-                break;
-            case USER_ERROR:
-                Toast.makeText(this, "授权操作遇到错误，请阅读Logcat输出", Toast.LENGTH_SHORT).show();
-                break;
-            case USER_OK:
-                Toast.makeText(this, "授权成功，正在跳转登录操作…", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
-        }
-        return false;
+    /**
+     * 从线程中执行分享到QQ空间的方法
+     * @param params 将分享事件的参数
+     */
+    private void doShareToQzone(final Bundle params) {
+        final Activity activity =MainActivity.this;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                MainActivity.mTencent.shareToQzone(activity, params, qZoneShareListener);
+            }
+        }).start();
     }
 
     /**
-     * 从线程中得到Bitmap用户头像
+     * 分享到QQ空间的接口回调
      */
-    class getUserBitmapThread extends Thread{
-        private String imageUrl ;
-        public getUserBitmapThread(String url){
-            imageUrl = url ;
+    IUiListener qZoneShareListener = new IUiListener() {
+        @Override
+        public void onCancel() {
+            //Util.toastMessage(QZoneShareActivity.this, "onCancel: ");
+            Toast.makeText(getApplicationContext(), "取消分享到空间", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        public void run() {
-            Bitmap bitmap = getHttpBitmap(imageUrl);
-            Message msg = getUserBitmapHandler.obtainMessage();
-            if(bitmap != null){
-                msg.obj = bitmap ;
-                msg.arg1 = 1 ;
-                msg.sendToTarget();
-            }else{
-                msg.obj  = null ;
-                msg.arg1 = 0 ;
-                msg.sendToTarget();
-            }
+        public void onError(UiError e) {
+            // TODO Auto-generated method stub
+            //Util.toastMessage(QZoneShareActivity.this, "onError: " + e.errorMessage, "e");
         }
-    }
 
-    /**
-     * 与线程对应的Handler处理器
-     */
-    Handler getUserBitmapHandler = new Handler(){
         @Override
-        public void handleMessage(Message msg) {
-            if(msg.arg1 == 1 && msg.obj != null){
-                Bitmap bitmap = (Bitmap) msg.obj;
-                headimage.setImageBitmap(bitmap);
-            }
+        public void onComplete(Object response) {
+            // TODO Auto-generated method stub
+            //Util.toastMessage(QZoneShareActivity.this, "onComplete: " + response.toString());
+            Toast.makeText(getApplicationContext(), "分享到空间成功", Toast.LENGTH_SHORT).show();
         }
+
     };
-    //  authorize()方法将引导用户在授权页面输入帐号密码，然后目标平台将验证此用户(此方法在此例子中仅仅是QQ账号登陆时候使用)
-    public void authorize(Platform plat) {
-        if (plat.isValid()) {
-            String userId = plat.getDb().getUserId();//获取id
-            if (!TextUtils.isEmpty(userId)) {   //如果用户已经存在了
-                UIHandler.sendEmptyMessage(IS_USER, this);
-                login(plat.getName(), userId, null);//不等于null执行登陆，讲用户id发送至目标平台进行验证
+
+    /**
+     * 接口基类
+     */
+    private class BaseUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object response) {
+            if (null == response) {
+                Log.e("ResponseError", "返回结果为空，登陆失败");
                 return;
             }
+            JSONObject jsonResponse = (JSONObject) response;
+            if (null != jsonResponse && jsonResponse.length() == 0) {
+                Log.e("ResponseError", "返回结果为空，登陆失败");
+                return;
+            }
+            doComplete((JSONObject) response);
         }
-        plat.setPlatformActionListener(this);
-        plat.SSOSetting(true);
-        plat.showUser(null);
-    }
-
-    /**
-     * 发送到Handler处理消息的方法
-     *
-     * @param plat     数据库用户基类
-     * @param userId   用户ID
-     * @param userInfo 用户数据库中的数据
-     */
-    public void login(String plat, String userId, HashMap<String, Object> userInfo) {
-        if(userInfo != null){
-            Message msg = new Message();
-            msg.what = USER_LOGIN;
-            msg.obj = userInfo;
-            UIHandler.sendMessage(msg, this);
+        protected void doComplete(JSONObject values) {
         }
-    }
-    //从URL中得到图片的方法
-    public static Bitmap getHttpBitmap(String url){
-        URL myFileURL;
-        Bitmap bitmap=null;
-        try{
-            myFileURL = new URL(url);
-            //获得连接
-            HttpURLConnection conn=(HttpURLConnection)myFileURL.openConnection();
-            //设置超时时间为6000毫秒，conn.setConnectionTiem(0);表示没有时间限制
-            conn.setConnectTimeout(6000);
-            //连接设置获得数据流
-            conn.setDoInput(true);
-            //不使用缓存
-            conn.setUseCaches(false);
-            //这句可有可无，没有影响
-            //conn.connect();
-            //得到数据流
-            InputStream is = conn.getInputStream();
-            //解析得到图片
-            bitmap = BitmapFactory.decodeStream(is);
-            //关闭数据流
-            is.close();
-        }catch(Exception e){
-            e.printStackTrace();
+        @Override
+        public void onError(UiError e) {
         }
-        return bitmap;
+        @Override
+        public void onCancel() {
+        }
     }
 }
